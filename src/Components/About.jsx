9dc167ef382b5ef4edf1837faa026016149f3d0d
Fernando Gorostieta Vargas <74213825+DmonID@users.noreply.github.com>
2023-08-30 14:33:35 +0000

    First setup change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Software Developement Engenier student studying Universidad Tecmilenio. I enjoy developing software solutions that are user-friendly, efficient, and scalable. I have experience in various programming languages, such as Java, Python, and C#, and I am always eager to learn new technologies and frameworks.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -36,7 +36,6 @@ const skillsList = [
   "Inclusive design",
   "Focus group testing",
   "Mobile user interfaces",
-  "Graphic design",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am enthusiastic about software engineering and I want to further develop my skills and knowledge in this field. I aim to be a successful software engineer.";
 
 const About = () => {
   return (
